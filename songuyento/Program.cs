a621c6730b0a6270cb92c93bab3588d8dea37eb5
Hideonkat <danghung0711dh@gmail.com>
2023-12-24 15:42:36 +0700

    gud

---
+++ songuyento/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace songuyento;
+
+ class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap 1 so di:");
+        int a = int.Parse(Console.ReadLine());
+        if(a<2)
+        {
+            Console.WriteLine("ko phai so nguyen to");
+        }
+        else
+        {
+            int b = 2;
+            bool c = true;
+            while (b<=Math.Sqrt(a))
+            {
+                if(a%b==0)
+                {
+                c=false;
+                break;
+                }
+                b++;              
+            }            
+            
+            if(c)            
+                Console.WriteLine(a + "la so nguyen to");
+            else
+                Console.WriteLine(a + "ko phai so nguyen to");
+        }
+        }
+        }
